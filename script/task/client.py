b84db00d40f006264ec270f079f58771914dedc3
mazorith <Mazorith@gmail.com>
2022-10-07 22:58:27 -0700

    Quick fix for metrics - Needs update

--- script/task/client.py
+++ script/task/client.py
@@ -1,12 +1,15 @@
 from socket import *
-from struct import pack
+from struct import pack, unpack
 import torch
 import pickle
+import time
+import sys
 
 class ClientProtocol:
 
     def __init__(self):
         self.socket = None
+        self.data = None
 
     def connect(self, server_ip, server_port):
         self.socket = socket(AF_INET, SOCK_STREAM)
@@ -17,35 +20,61 @@ class ClientProtocol:
         self.socket.close()
         self.socket = None
 
-    def send_tensor(self, tensor_data):
-        length = pack('>Q', len(tensor_data))
+    def send_encoder_data(self, data):
+        data = pickle.dumps(data)
+        length = pack('>Q', len(data))
 
         self.socket.sendall(length)
-        self.socket.sendall(tensor_data)
+        self.socket.sendall(data)
 
         ack = self.socket.recv(1)
+        #print(ack)
 
-# if __name__ == '__main__':
-#     cp = ClientProtocol()
+    def client_handshake(self):
+        print('Sending handshake to server')
+        self.socket.sendall(b'\00')
+        ack = self.socket.recv(1)
+        if(ack == b'\00'):
+            print('Successfully recived server Handshake')
+        else:
+            print('Message recived not server ack')
 
-#     image_data = None
-#     with open('IMG_0077.jpg', 'r') as fp:
-#         image_data = fp.read()
+    def handle_input_data(self):
+        collected_message = False
+        while not collected_message:
+            bs = self.socket.recv(8)
+            (length,) = unpack('>Q', bs)
+            data = b''
+            while len(data) < length:
+                to_read = length - len(data)
+                data += self.socket.recv(
+                    4096 if to_read > 4096 else to_read)
 
-#     assert(len(image_data))
-#     cp.connect('127.0.0.1', 55555)
-#     cp.send_image(image_data)
-#     cp.close()
+            # send our 0 ack
+            assert len(b'\00') == 1
+            collected_message = True
+            self.socket.sendall(b'\00')
+            self.data = pickle.loads(data)
 
-# class ProcessData:
-#     process_id = 0
-#     project_id = 0
-#     task_id = 0
-#     start_time = 0
-#     end_time = 0
-#     user_id = 0
-#     weekend_id = 0
+#main functionality for testing/debugging
+if __name__ == '__main__':
 
+    cp = ClientProtocol()
+
+    tensor = torch.rand([200,200,200])
+    print(tensor.dtype)
+    print(tensor.element_size(), tensor.nelement(), tensor.element_size() * tensor.nelement())
+    #tensor = pickle.dumps(tensor)
+    cp.connect('128.195.54.126', 55555) #128.195.54.126, 127.0.0.1
+    cp.client_handshake()
+    #time.sleep(30)
+    cp.send_encoder_data(tensor)
+    cp.handle_input_data()
+    print(cp.data.shape)
+    print(cp.data.element_size(), cp.data.nelement(), cp.data.element_size() * cp.data.nelement())
+    print(sys.getsizeof(cp.data))
+    print(pack('>Q', len(cp.data)))
+    cp.close()
 
 # HOST = 'localhost'
 # PORT = 50007
--- script/task/server.py
+++ script/task/server.py
@@ -1,60 +1,87 @@
+from multiprocessing import connection
+from sqlite3 import connect
 import torch
 import pickle
+import time
+import sys
 from socket import *
-from struct import unpack
+from struct import pack, unpack
 
 class ServerProtocol:
 
     def __init__(self):
         self.socket = None
+        self.connection = None
+        self.data = None
 
     def listen(self, server_ip, server_port):
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.bind((server_ip, server_port))
         self.socket.listen(1)
 
-    def handle_tensor(self):
+    def handle_encoder_data(self):
+        collected_message = False
+        while not collected_message:
+            bs = self.connection.recv(8)
+            (length,) = unpack('>Q', bs)
+            data = b''
+            while len(data) < length:
+                to_read = length - len(data)
+                data += self.connection.recv(
+                    4096 if to_read > 4096 else to_read)
 
-        try:
-            while True:
-                (connection, addr) = self.socket.accept()
-                try:
-                    bs = connection.recv(8)
-                    (length,) = unpack('>Q', bs)
-                    data = b''
-                    while len(data) < length:
-                        to_read = length - len(data)
-                        data += connection.recv(
-                            4096 if to_read > 4096 else to_read)
+            # send our 0 ack
+            assert len(b'\00') == 1
+            collected_message = True
+            self.connection.sendall(b'\00')
+            self.data = pickle.loads(data)
+        
+    def server_handshake(self):
+        (connection, addr) = self.socket.accept()
+        self.connection = connection
+        handshake = self.connection.recv(1)
+        if(handshake == b'\00'):
+            print('Successfully recived client Handshake')
+            print('Sending handshake ack')
+            connection.sendall(b'\00')
+        else:
+            print('Message recived not client handshake')
 
-                    # send our 0 ack
-                    assert len(b'\00') == 1
-                    connection.sendall(b'\00')
-                finally:
-                    connection.shutdown(SHUT_WR)
-                    connection.close()
-        finally:
-            self.close()
+    def send_input_data(self, data):
+        data = pickle.dumps(data)
+        length = pack('>Q', len(data))
+
+        self.connection.sendall(length)
+        self.connection.sendall(data)
+
+        ack = self.connection.recv(1)
+        #print(ack)
 
     def close(self):
+        self.connection.shutdown(SHUT_WR)
+        self.connection.close()
         self.socket.close()
         self.socket = None
 
-# if __name__ == '__main__':
-#     sp = ServerProtocol()
-#     sp.listen('127.0.0.1', 55555)
-#     sp.handle_tensor()
+#main functionality for testing/debugging
+if __name__ == '__main__':
+    sp = ServerProtocol()
+    sp.listen('128.195.54.126', 55555)
+    sp.server_handshake()
+    #time.sleep(30)
+    #sp.listen('127.0.0.1', 55556)
+    sp.handle_encoder_data()
+    print(sp.data.shape)
 
+    print(sys.getsizeof(sp.data))
+    print(pack('>Q', len(sp.data)))
+    tensor = torch.rand([100,100,100])
+    #tensor = pickle.dumps(tensor)
 
-# class ProcessData:
-#     process_id = 0
-#     project_id = 0
-#     task_id = 0
-#     start_time = 0
-#     end_time = 0
-#     user_id = 0
-#     weekend_id = 0
-
+    sp.send_input_data(tensor)
+    print(sys.getsizeof(torch.rand([10,10,10])))
+    sp.close()
+    
 
 # HOST = 'localhost'
 # PORT = 50007
